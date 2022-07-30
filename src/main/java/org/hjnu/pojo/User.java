a4368e58ddb4f76f62031bc419e529972e4186a8
bob <zoubobo@outlook.com>
2022-07-30 21:23:10 +0800

    first commit 1.3

--- src/main/java/org/hjnu/pojo/User.java
+++ src/main/java/org/hjnu/pojo/User.java
@@ -13,4 +13,20 @@ public class User implements Serializable {
     public void setId(Integer id) {
         this.id = id;
     }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
