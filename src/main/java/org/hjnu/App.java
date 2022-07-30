285e2cf3b4ff54ce2ef9dc00ebe6e7e420822422
bob <zoubobo@outlook.com>
2022-07-30 19:29:57 +0800

    first commit

---
+++ src/main/java/org/hjnu/App.java
@@ -0,0 +1,14 @@
+package org.hjnu;
+
+/**
+ * Hello world!
+ *
+ * @author 36017
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Git!" );
+    }
+}
